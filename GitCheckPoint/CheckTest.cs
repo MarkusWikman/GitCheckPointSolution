957d2dd635590dcb42b4e5064b79bc2124bb7fa7
Markus Wikman <Wikman.markus@gmail.com>
2023-02-16 15:11:20 +0100

    Add project files.

---
+++ GitCheckPoint/CheckTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCheckPoint
+{
+    internal class CheckTest
+    {
+        public void testMessage(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ GitCheckPoint/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GitCheckPoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the Git checkpoint task 1.1");
+            CheckTest checkTest = new CheckTest();
+            checkTest.testMessage("Task 2 checked");
+        }
+    }
+}
